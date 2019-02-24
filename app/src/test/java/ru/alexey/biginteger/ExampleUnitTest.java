1720188cdb8de2a2a67b558a8dea50ae7040e3b0
alexleru <alexleru@yandex.ru>
2019-02-24 23:25:25 +0300

    test

--- app/src/test/java/ru/alexey/biginteger/ExampleUnitTest.java
+++ app/src/test/java/ru/alexey/biginteger/ExampleUnitTest.java
@@ -1,6 +1,12 @@
 package ru.alexey.biginteger;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+import ru.alexey.biginteger.Calculation.PrimeNumbers;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +16,39 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    ArrayList<BigInteger> bigIntegers;
+    PrimeNumbers primeNumbers;
+    ArrayList<BigInteger> resultBigIntegers;
+    @Before
+    public  void beforePrime(){
+        bigIntegers = new ArrayList<>();
+        bigIntegers.add(new BigInteger("3"));
+        primeNumbers = new PrimeNumbers(bigIntegers);
+
+        resultBigIntegers = new ArrayList<>();
+        resultBigIntegers.add(new BigInteger("3"));
+        resultBigIntegers.add(new BigInteger("5"));
+        resultBigIntegers.add(new BigInteger("7"));
+        resultBigIntegers.add(new BigInteger("11"));
+        resultBigIntegers.add(new BigInteger("13"));
+    }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testPrime() {
+        primeNumbers.loop();
+        bigIntegers = primeNumbers.getBigIntegerArrayList();
+
+        Boolean result = true;
+        for (int i = 0; i < bigIntegers.size(); i++){
+
+            String bigInteger = bigIntegers.get(i).toString();
+            String resulteBigInteger = resultBigIntegers.get(i).toString();
+
+            if (!bigInteger.equals(resulteBigInteger)){
+                result = false;
+                break;
+            }
+        }
+        assertTrue("test: ", result);
     }
 }
